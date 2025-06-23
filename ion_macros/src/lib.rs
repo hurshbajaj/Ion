56ceec9310e926df82ca902731e8492fbc53bcaa
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-06-23 18:53:55 +0530

    Ion_Macros cleanup

--- ion_macros/src/lib.rs
+++ ion_macros/src/lib.rs
@@ -30,34 +30,29 @@ pub fn Stmt(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn Expr(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let node_type = attr.clone();       
+    let item_clone = item.clone();      
+
+    let stmt_expanded = Stmt(node_type, item_clone);
+    let stmt_expanded2: proc_macro2::TokenStream = stmt_expanded.into();
+
     let input = parse_macro_input!(item as DeriveInput);
-    let node_type = parse_macro_input!(attr as ExprPath);
     let name = &input.ident;
 
-    let expanded = quote! {
-        #[derive(Debug, Clone)]
-        #input
-
+    let expr_expanded = quote! {
         impl Expr for #name {
             fn clone_box_expr(&self) -> Box<dyn Expr> {
                 Box::new(self.clone())
             }
         }
-
-        impl Stmt for #name {
-            fn kind(&self) -> NodeType {
-                #node_type
-            }
-            fn as_any(&self) -> &dyn Any {
-                self
-            }
-            fn clone_box(&self) -> Box<dyn Stmt> {
-                Box::new(self.clone())
-            }
-        }
     };
 
-    TokenStream::from(expanded)
+    let combined = quote! {
+        #stmt_expanded2
+        #expr_expanded
+    };
+
+    TokenStream::from(combined)
 }
 
 
